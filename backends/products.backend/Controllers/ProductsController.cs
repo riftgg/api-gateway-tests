3335a484701437bdcb488a5e5cb81a82908fd023
Victor Martos <vmartos@outlook.com>
2019-07-10 12:48:07 +0200

    Add tests backends users and products

---
+++ backends/products.backend/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace products.backend.Controllers
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products;
+
+        public ProductsController()
+        {
+            products = new List<Product>
+            {
+                new Product { ProductId = 1, Name = "Ocelot" },
+                new Product { ProductId = 2, Name = "Krakend" },
+            };
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(products);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ backends/users.backend/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace users.backend.Controllers
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        List<User> users;
+
+        public UsersController()
+        {
+            users = new List<User>
+            {
+                new User { UserId = 1, Name = "Ocelot"},
+                new User { UserId = 2, Name = "Krakend"}
+            };
+        }
+
+        // GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return Ok(users);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = users.FirstOrDefault(u => u.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
