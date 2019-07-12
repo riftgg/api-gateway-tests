695ecda903a53684161a7ffdd8906ba99f67d5b8
Victor Martos <vmartos@outlook.com>
2019-07-12 14:18:35 +0200

    Add a jmeter test file and an sh script
    - the sh scripts starts all containers using the docker compose file
    it starts the ocelot and krakend tests using jmeter and finally it
    starts the custom performance tests done by us.
    - you can see the final results for jmeter performance tests at
    perf-dashboard-ocelot and perf-dashboard-krakend

---
+++ start-perf-test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm results-krakend
+rm results-ocelot
+echo "Executing docker-compose in detach mode"
+docker-compose up --build --detach
+echo "starting ocelot performance test using jmeter"
+$1 -n -t api-gateway-perf-test.jmx -l results-ocelot -e -o perf-dashboard-ocelot -JPort=8081
+echo "you can view ocelot results in perf-dashboard-ocelot folder"
+echo "starting krakend performance test using jmeter"
+$1 -n -t api-gateway-perf-test.jmx -l results-krakend -e -o perf-dashboard-krakend -JPort=8082
+echo "you can view krakend results in perf-dashboard-krakend folder"
+echo "\n\nStarting .net core perf test"
+dotnet run --project perf-test/PerfTest.csproj
+echo "stopping docker compose"
+docker-compose stop
