f66daa62c3ca6e8c68307c6e6810ba49d0e6a43d
Victor Martos <vmartos@outlook.com>
2019-07-12 14:58:03 +0200

    add batch script & modify bash script

--- start-perf-test.bat
+++ start-perf-test.bat
@@ -1,16 +1,15 @@
-#!/bin/sh
 echo "Cleaning old resources"
-rm results-krakend
-rm results-ocelot
-rm -rf perf-dashboard-ocelot
-rm -rf perf-dashboard-krakend
+del results-krakend
+del results-ocelot
+rmdir /s /q perf-dashboard-ocelot
+rmdir /s /q perf-dashboard-krakend
 echo "Executing docker-compose in detach mode"
 docker-compose up --build --detach
 echo "starting ocelot performance test using jmeter"
-$1 -n -t api-gateway-perf-test.jmx -l results-ocelot -e -o perf-dashboard-ocelot -JPort=8081
+%1 -n -t api-gateway-perf-test.jmx -l results-ocelot -e -o perf-dashboard-ocelot -JPort=8081
 echo "you can view ocelot results in perf-dashboard-ocelot folder"
 echo "starting krakend performance test using jmeter"
-$1 -n -t api-gateway-perf-test.jmx -l results-krakend -e -o perf-dashboard-krakend -JPort=8082
+%1 -n -t api-gateway-perf-test.jmx -l results-krakend -e -o perf-dashboard-krakend -JPort=8082
 echo "you can view krakend results in perf-dashboard-krakend folder"
 echo "\n\nStarting .net core perf test"
 dotnet run --project perf-test/PerfTest.csproj
